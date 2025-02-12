f4e641555a6e763af5a50e3c199d23bccb1ea8ea
Fanibhushan-Tripathy <fanibhushan1140.becse24@chitkara.edu.in>
2025-02-12 11:14:20 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 int main () {
-    char a[21] = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"};
-    char b[8] = {"!","@","#","$","%","^","&","*"};
-    char c[10] = {"1","2","3","4","5","6","7","8","9"};
-    char e[5] = {"a","e","i","o","u"};
+    char a[] = "bcdfghjklmnpqrstvwxyz";
+    char b[] = "!@#$%^&*";
+    char c[] = "aeiou";
+    char e[] = "0123456789";
     char d;
     scanf("%c", &d);
-    
-    for(int i = 0; i < 21; i++) {
+
+    for(int i = 0; a[i] != '\0'; i++) {
         if (a[i] == tolower(d)){
             printf("Consonant");
         }
     }
-    for(int i = 0; i < 8; i++) {
+    for(int i = 0; b[i] < '\0' ; i++) {
         if (b[i] == d){
             printf("Special Character");
         }
     }
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; e[i] != '\0'; i++) {
         if (c[i] == d){
             printf("Digit");
         }
     }
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; c[i] != '\0'; i++) {
         if (e[i] == tolower(d)){
             printf("Vowel");
         }

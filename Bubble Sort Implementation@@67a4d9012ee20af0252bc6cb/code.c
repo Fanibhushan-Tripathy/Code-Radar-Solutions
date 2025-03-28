7d79ddd0b7ade7e86d6dfe6b46503711b5bbb91b
Fanibhushan-Tripathy <fanibhushan1140.becse24@chitkara.edu.in>
2025-03-28 10:18:46 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,44 +1,19 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-void prArr(int* arr[], int numb){
-    for(int i = 0; i < numb; i++){
-        printf("%d ", *arr[i]);
-    }
-}
-
-void swap(int* a, int* b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void BsortArr(int* arr[], int numb){
-    for (int i = 0; i < numb - 1; i++){
-        for (int j = 0; j < numb - i - 1; j++){
-            if (*arr[j] > *arr[j+1]){
-                swap(arr[j], arr[j+1]);
+void bubbleSort(arr, n){
+    for(int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - j - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    prArr(arr,numb);
 }
 
-
-int main() {
-    int numb;
-    scanf("%d", &numb);
-    int** arr = (int**)malloc(numb * sizeof(int*));
-    for (int i = 0; i < numb; i++){
-        arr[i] = (int* )malloc(sizeof(int));
-        scanf("%d", arr[i]);
+void printArray(arr, n){
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i])
     }
-    BsortArr(arr,numb);
-    
-    // Free allocated memory
-    for (int i = 0; i < numb; i++){
-        free(arr[i]);
-    }
-    free(arr);
-    return 0;
 }
